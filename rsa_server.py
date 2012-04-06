25f574d5b9b3ad794fcaa5b6ffc0f66a0442e823
epc2101 <ecoop@seas.upenn.edu>
2012-04-05 23:23:14 -0300

    Corrected main method

--- rsa_server.py
+++ rsa_server.py
@@ -4,13 +4,10 @@
    Qing Xie"""
 
 import socket
-import SocketServer
 import threading
 import Queue
 
 QUEUE = Queue()
-#PUT HOST & PORT HERE
-#What protocol are we using? 
 
 
 def process_queue(client_socket, message):
@@ -21,6 +18,7 @@ class QueueThread(threading.Thread):
     """Decodes and processes client messages."""
     def __init__(self):
         threading.Thread.__init__(self)
+        self.daemon = True
 
     def run(self):
         """Get each message off the queue and process it."""
@@ -42,39 +40,27 @@ def ConnectionHandler(threading.Thread):
 def main():
     """Start the server and spawn threads to handle each request"""            
     # Set up the listening socket
-    #http://docs.python.org/library/socketserver.html use as sample TCP
-    
-    HOST = 'localhost'
-    PORT = 8282 #random port number, can change this
-    listen_socket = SocketServer.TCPServer((HOST, PORT), ConnectionHandler)
-    #listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    #listen_socket.bind((HOST, PORT))
-    #listen_socket.listen(1)
-    
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
 
     # Start the thread to process the queue
     QueueThread().start()
     
     # Accept connections
-   try:
-      listen_socket.server_forever()
-   except KeyboardInterrupt:
-      pass
-   finally:
-      listen_socket.close()
-    
-   """ try:
+    try:
         while True: 
             client, address = listen_socket.accept()
             #Spawn a thread as each client connects
-            client_connection = ConnectionHandler(client)    
-            client_connection.start()
+            conn = ConnectionHandler(client)    
+            conn.start()
     except KeyboardInterrupt:
         pass
     finally:
-        listen_socket.close()"""
+        listen_socket.close()
     
     
 if __name__ == "__main__":
     main()
+
