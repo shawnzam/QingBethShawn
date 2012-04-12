3273870b0a7fb911f2bee150b7cedd1c17f679f2
Shawn Zamechek <shawnzam@gmail.com>
2012-04-12 15:37:05 -0400

    started work on client, sending a message over a socket to a port is working

---
+++ client.py
@@ -0,0 +1,27 @@
+"""Chat server - RSA Assignment.
+   Beth Cooper
+   Shawn Zamechek
+   Qing Xie"""
+
+
+import socket
+import threading
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def myconnect(host, port):
+    s.connect((host, port))
+    
+def send(message):
+    totalsent = 0
+    formattedMessage = "message: " + message + "\n\n"    
+    while (totalsent < len(formattedMessage)):
+        sent = s.send(formattedMessage[totalsent:])
+        if sent == 0:
+            raise RuntimeError("connection broken")
+        totalsent = totalsent + sent
+        print message
+
+
+myconnect("", 8888)
+send("foobar")
