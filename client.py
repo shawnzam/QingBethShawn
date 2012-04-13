6e70fc7ff963355c234d2f48301149962b7b4493
Shawn Zamechek <shawnzam@gmail.com>
2012-04-13 11:54:57 -0400

    added some rough encrypt/decryption to the client

--- client.py
+++ client.py
@@ -6,26 +6,38 @@
 
 import socket
 import threading
+import rsa
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def myconnect(host, port):
     s.connect((host, port))
     
-def send(message):
+def send(message, keys):
     totalsent = 0
-    formattedMessage = "message: " + message + "\n\n"    
-    while (totalsent < len(formattedMessage)):
-        sent = s.send(formattedMessage[totalsent:])
+    formattedMessage = "message: " + message + "\n\n"
+    encryptedMessage = ''
+    for c in formattedMessage:
+            encryptedMessage += str(rsa.encrypt(c, keys[0])) + "," 
+    while (totalsent < len(encryptedMessage)):
+        sent = s.send(encryptedMessage[totalsent:])
         if sent == 0:
             raise RuntimeError("connection broken")
         totalsent = totalsent + sent
-        print message
-
+        print encryptedMessage
+        return encryptedMessage
 
 def main():
+   keys = rsa.initializeKeys()
    myconnect("", 8888)
-   send("foobar")
+   msg = send("foobar", keys)
+   decode = ''
+   msgList = msg.split(",")
+   del msgList[-1]     
+   for c in msgList:
+        decode += rsa.decrypt(int(c), keys[1])
+   print decode       
 
 if __name__ == "__main__":
     main()
+
